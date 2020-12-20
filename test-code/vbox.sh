7fbbeb0a0ee7b6341ff0467d89bf2606e36e3286
Your Name <you@example.com>
2020-12-20 14:03:01 -0800

    vbox.sh: setuuid added, being tested, not working yet.

--- test-code/vbox.sh
+++ test-code/vbox.sh
@@ -11,9 +11,8 @@ if [[ $P1 == "stop" ]] ; then
 	vboxmanage storageattach $VM_NAME --type hdd --medium None  --storagectl SATA --device 0 --port 1
 elif [[ $P1 == "start" ]] ; then
         echo "attaching hdd vbox vm..."
-	vboxmanage storageattach $VM_NAME --type hdd --medium $IMAGE_NAME   --storagectl SATA --device 0 --port 1
+	vboxmanage storageattach $VM_NAME --type hdd --medium $IMAGE_NAME   --storagectl SATA --device 0 --port 1 --setuuid  6932e31d26e6419d95f04f6b5dd257ee
 	virtualbox -startvm $VM_NAME -dbg
 else
         echo "invalid parameter: $P1 "
 fi
-
