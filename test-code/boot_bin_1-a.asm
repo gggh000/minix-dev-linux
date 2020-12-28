f21c32a4a40568aa6a18e12e8d410d4c722b6438
Guyen Gankhuyag <guyen.gankhuyag@amd.com>
2020-12-28 00:48:06 -0500

    checkpoint: can build boot.elf.bin using both C and asm with main in asm.

--- test-code/boot_bin_1-a.asm
+++ test-code/boot_bin_1-a.asm
@@ -1,19 +1,28 @@
-; -----------------------------------------------------------------------------
-; A 64-bit function that returns the maximum value of its three 64-bit integer
-; arguments.  The function has signature:
-;
-;   int64_t maxofthree(int64_t x, int64_t y, int64_t z)
-;
-; Note that the parameters have already been passed in rdi, rsi, and rdx.  We
-; just have to return the value in rax.
-; -----------------------------------------------------------------------------
+;   https://cs.lmu.edu/~ray/notes/nasmtutorial/
+extern      functionC
+extern      functionC2int
+extern      functionC2long
+    global    putchar1
+    global    main
 
-        global  maxofthree
-        section .text
-maxofthree:
-        mov     rax, rdi                ; result (rax) initially holds x
-        cmp     rax, rsi                ; is x less than y?
-        cmovl   rax, rsi                ; if so, set result to y
-        cmp     rax, rdx                ; is max(x,y) less than z?
-        cmovl   rax, rdx                ; if so, set result to z
-        ret                             ; the max will be in rax
+            section   .text
+_start1:     
+main:
+	sub	ax, ax
+	mov	ds, ax
+        mov     al, '@'
+        mov     ah, 0xe
+        int     0x10
+	jmp	$
+	call	functionC
+	mov	ax, 0x100
+	call	functionC2int
+
+; a function "declaration" in assembly
+
+putchar1:
+	sub	bx, bx
+	jmp	$
+otherFunction: 
+
+            section   	.data
